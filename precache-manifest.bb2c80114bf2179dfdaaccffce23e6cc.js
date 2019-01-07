7b024a16ef8b77de815f3f2d39438a50719d77f0
leamsigc <leamsigc@hotmail.com>
2019-01-06 23:20:42 -0500

    Updates

---
+++ precache-manifest.bb2c80114bf2179dfdaaccffce23e6cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f58aa42e7ebeff36563c",
+    "url": "/Todo-app/static/css/main.38b0c8e9.chunk.css"
+  },
+  {
+    "revision": "f58aa42e7ebeff36563c",
+    "url": "/Todo-app/static/js/main.f58aa42e.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/Todo-app/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "ce0302b16508312457e7",
+    "url": "/Todo-app/static/js/runtime~main.ce0302b1.js"
+  },
+  {
+    "revision": "514ef8d1b8e9edfeb49ab1df777d135e",
+    "url": "/Todo-app/index.html"
+  }
+];
